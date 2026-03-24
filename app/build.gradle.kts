6fc8119817d017bb8d442d41d05dc4d0dbdec08b
User <kkdwu8@gmail.com>
2026-03-24 15:28:40 +0800

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 
 dependencies {
     implementation("com.google.code.gson:gson:2.10")
-    implementation("com.squareup.okhttp3:okhttp:5.3.2")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
